1460a89474bec24c15c53863e354ac9129c4c12f
b4r7x <b4r7dev@gmail.com>
2026-03-06 19:49:35 +0100

    cleanup components

--- src/cli/utils/commands.ts
+++ src/cli/utils/commands.ts
@@ -1,27 +1,23 @@
 import { resolve } from "node:path";
-import { loadResolvedConfig, getManifestHooks, type ResolvedConfig } from "./config.js";
-import { getRegistryItem, getRelativePath, type RegistryItem } from "./registry.js";
+import { loadResolvedConfig, getManifestHooks } from "./config.js";
+import { getRegistryItem, getRelativePath } from "./registry.js";
 import {
-  createRequireConfig,
+  createItemAccessors,
   createInstallChecker,
-  getItemOrThrow as coreGetItemOrThrow,
-  validateItems as coreValidateItems,
 } from "@b4r7/cli-core";
 import { CONFIG_FILE, ITEM_LABEL, LIST_COMMAND, INIT_COMMAND } from "../constants.js";
 
-export const requireConfig = createRequireConfig<unknown, ResolvedConfig>({
-  configFileName: CONFIG_FILE,
-  initCommand: INIT_COMMAND,
-  loadResolved: loadResolvedConfig,
-});
+const { requireConfig, getOrThrow: getHookOrThrow, validate: validateHooks } =
+  createItemAccessors({
+    configFileName: CONFIG_FILE,
+    initCommand: INIT_COMMAND,
+    itemLabel: ITEM_LABEL,
+    listCommand: LIST_COMMAND,
+    loadResolved: loadResolvedConfig,
+    getItem: getRegistryItem,
+  });
 
-export function getHookOrThrow(name: string): RegistryItem {
-  return coreGetItemOrThrow(name, getRegistryItem, ITEM_LABEL, LIST_COMMAND);
-}
-
-export function validateHooks(names: string[]): void {
-  coreValidateItems(names, getRegistryItem, ITEM_LABEL, LIST_COMMAND);
-}
+export { requireConfig, getHookOrThrow, validateHooks };
 
 export function createHookInstallChecker(cwd: string, hooksFsPath: string): (name: string) => boolean {
   return createInstallChecker({
